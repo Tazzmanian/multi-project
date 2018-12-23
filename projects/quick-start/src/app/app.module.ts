06c3698c8147ee81dcb37160303f8ba29c2fb05f
Tazzmanian <banskaliqtabg@yahoo.com>
2018-12-23 17:42:08 +0200

    add bootstrap and ng-bootstrap

--- projects/quick-start/src/app/app.module.ts
+++ projects/quick-start/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StocksComponent } from './stocks/stocks.component';
@@ -20,7 +22,8 @@ import { BondsDirective } from './bonds.directive';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
