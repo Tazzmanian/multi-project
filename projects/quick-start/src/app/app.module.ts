e84c72bf24223fab514addd0b84832e9c71fb522
Tazzmanian <banskaliqtabg@yahoo.com>
2018-12-23 15:01:24 +0200

    directive

--- projects/quick-start/src/app/app.module.ts
+++ projects/quick-start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { HighlightDirective } from './shared/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StocksComponent
+    StocksComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
