ffc463b90a293dfcd3392ac86e674680bc5818d0
Tazzmanian <tazzmanian@abv.bg>
2019-03-06 22:22:28 +0200

    core module

--- projects/angular2-complete-guide/src/app/app.module.ts
+++ projects/angular2-complete-guide/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -12,20 +11,19 @@ import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.modul';
 
 @NgModule({
    declarations: [
       AppComponent,
-      HeaderComponent,
-      HomeComponent,
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       AppRoutingModule,
       SharedModule,
-      AuthModule
+      AuthModule,
+      CoreModule
    ],
    providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
    bootstrap: [
